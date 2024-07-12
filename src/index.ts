7ec755df167ba8247e720332b370c101f05882fc
Hyeonki Hong <hhk7734@gmail.com>
2024-07-12 18:57:44 +0900

    feat: add commander

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import Koa from "koa";
 import Router from "@koa/router";
 import dotenv from "dotenv";
-import { logger } from "@/pkg/logger";
+import { program } from "commander";
+import * as logger from "@/pkg/logger";
 import { loggerMiddleware } from "@/userinterface/restapi/middleware/logger";
 import { requestIDMiddleware } from "./userinterface/restapi/middleware/requestID";
 
 dotenv.config();
+
+logger.addOption(program);
+
+program.parse();
+
+const opts = program.opts();
+
+logger.setGlobalLoggerWith(opts);
 
 const app = new Koa();
 const router = new Router();
@@ -22,5 +31,5 @@ router.get("/", (ctx) => {
 app.use(router.routes());
 
 app.listen(3000, () => {
-	logger().info("start");
+	logger.logger().info("start");
 });
--- src/pkg/logger.ts
+++ src/pkg/logger.ts
@@ -1,22 +1,57 @@
 import { AsyncLocalStorage } from "node:async_hooks";
-import pino from "pino";
+import pino, { LoggerOptions } from "pino";
+import { Option, Command, OptionValues } from "commander";
 
-const asyncLocalStorage = new AsyncLocalStorage<object>();
-const _logger = pino({
+const LOG_LEVEL_KEY = "log_level";
+const LOG_FORMAT_KEY = "log_format";
+
+const defaultOpts: LoggerOptions = {
 	formatters: {
 		level: (label) => ({ level: label }),
 		bindings: () => ({}),
 	},
 	timestamp: pino.stdTimeFunctions.isoTime,
-});
+};
+
+const asyncLocalStorage = new AsyncLocalStorage<object>();
+let globalLogger = pino(defaultOpts);
+
+export function addOption(cmd: Command) {
+	cmd.addOption(
+		new Option(`--${LOG_LEVEL_KEY} <level>`, `Log level`)
+			.env(LOG_LEVEL_KEY.toUpperCase())
+			.choices(["debug", "info", "warn", "error"])
+			.default("info"),
+	);
+	cmd.addOption(
+		new Option(`--${LOG_FORMAT_KEY} <format>`, `Log format`)
+			.env(LOG_FORMAT_KEY.toUpperCase())
+			.choices(["json", "pretty"])
+			.default("json"),
+	);
+}
+
+export function setGlobalLoggerWith(opts: OptionValues) {
+	globalLogger.info(
+		{ config: { [LOG_LEVEL_KEY]: opts[LOG_LEVEL_KEY], [LOG_FORMAT_KEY]: opts[LOG_FORMAT_KEY] } },
+		"logger config",
+	);
+
+	const options = { ...defaultOpts, level: opts[LOG_LEVEL_KEY] };
+	if (opts[LOG_FORMAT_KEY] === "pretty") {
+		options.transport = { target: "pino-pretty", options: { singleLine: true } };
+	}
+
+	globalLogger = pino(options);
+}
 
 export function logger() {
 	const store = asyncLocalStorage.getStore();
 	if (store) {
-		return _logger.child(store);
+		return globalLogger.child(store);
 	}
 
-	return _logger;
+	return globalLogger;
 }
 
 export function withFields(fields: object, callbacke: () => Promise<void> | void) {
